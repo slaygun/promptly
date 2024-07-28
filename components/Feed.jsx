74d2789c1a7d972a185309f1a6dcfaa473b44a15
slaygun <nabhraghav@gmail.com>
2024-07-28 22:44:08 +0530

    no cache added to feed fetching

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ export const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
+      const response = await fetch('/api/prompt', {cache:'no-store'});
       const data = await response.json();
       setPosts(data);
     }
